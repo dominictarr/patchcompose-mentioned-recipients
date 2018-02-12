9e003464d56c575691cefb38a7d2e245297be088
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-12 13:34:49 +1300

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# patchcompose-mentioned-recipients
+
+adds recipients to patchcompose, the way that patchbay does
+it, anyone mentioned in the first post in a thread is a recipient.
+
+should be used after `patchcompose-legacy-mentions`
+(or something else which provides mentions)
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,29 @@
+var isArray = Array.isArray
+var isFeed = require('ssb-ref').isFeed
+
+exports.gives = {
+  compose: {
+    post: true
+  }
+}
+
+exports.needs = {
+  identity: {
+    main: 'first'
+  }
+}
+
+exports.create = function (api) {
+  return {compose: {post:
+    function (content, context) {
+      if(content.recps && content.recps.length === 0 && isArray(content.mentions))
+        content.recps = content.mentions.map(function (e) {
+          return isFeed(e) ? e : isFeed(e.link) ? e.link : null
+        }).filter(Boolean).concat(api.identity.main())
+      return content
+    }
+  }}
+}
+
+
+
